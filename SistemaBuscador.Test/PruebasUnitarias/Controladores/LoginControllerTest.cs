dcbae6f3644767c2addfde78a5b8ab2b0e592daa
Bruno Matus <brumatuz03@gmail.com>
2021-10-04 01:28:22 -0300

    Clase 04/10/2021 En Pausa

--- SistemaBuscador.Test/PruebasUnitarias/Controladores/LoginControllerTest.cs
+++ SistemaBuscador.Test/PruebasUnitarias/Controladores/LoginControllerTest.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using SistemaBuscador.Controllers;
 using SistemaBuscador.Models;
+using SistemaBuscador.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -31,10 +33,17 @@ namespace SistemaBuscador.Test.PruebasUnitarias.Controladores
         public async Task LoginUsuarioNoExiste()
         {
             //Preparacion
-            var loginService = new LoginRepositoryEFFalse();
+            //var loginService = new LoginRepositoryEFFalse();
+            var loginService = new Mock<ILoginRepository>();
+            loginService.Setup(x => 
+            x.UserExist(
+                It.IsAny<string>(),
+                It.IsAny<string>()))
+                .Returns(Task.FromResult(false));
+
             var model = new LoginViewModel() { Usuario = "Usuario1", Password = "Password1" };
             //Ejecucion
-            var controller = new LoginController(loginService);
+            var controller = new LoginController(loginService.Object);
             var resultado = await controller.Login(model) as ViewResult;
             //Validacion
             Assert.AreEqual(resultado.ViewName, "Index");
@@ -44,10 +53,16 @@ namespace SistemaBuscador.Test.PruebasUnitarias.Controladores
         public async Task LoginUsuarioExiste()
         {
             //Preparacion
-            var loginService = new LoginRepositoryEFTrue();
+            //var loginService = new LoginRepositoryEFTrue();
+            var loginService = new Mock<ILoginRepository>();
+            loginService.Setup(x =>
+            x.UserExist(
+                It.IsAny<string>(),
+                It.IsAny<string>()))
+                .Returns(Task.FromResult(true));
             var model = new LoginViewModel() { Usuario = "Usuario1", Password = "Password1" };
             //Ejecucion
-            var controller = new LoginController(loginService);
+            var controller = new LoginController(loginService.Object);
             var resultado = await controller.Login(model) as RedirectToActionResult;
             //Validacion
             Assert.AreEqual(resultado.ActionName, "Index");
