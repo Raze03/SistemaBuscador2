fdee34bcaa3f3184ef0efd014a5bcbab24512e88
Bruno Matus <brumatuz03@gmail.com>
2021-10-09 02:48:34 -0300

    Clase 09/10/2021 Cambiar Contraseña y Editar usuarios

--- SistemaBuscador/Repositories/UsuarioRepository.cs
+++ SistemaBuscador/Repositories/UsuarioRepository.cs
@@ -1,4 +1,5 @@
-﻿using SistemaBuscador.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaBuscador.Entities;
 using SistemaBuscador.Models;
 using SistemaBuscador.Utilidades;
 using System;
@@ -30,6 +31,62 @@ namespace SistemaBuscador.Repositories
             };
 
             _context.Usuarios.Add(nuevoUsuario);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<UsuarioListaModel>> ObtenerListaUsuarios()
+        {
+            var respuesta = new List<UsuarioListaModel>();
+            var listaDelaBd = await _context.Usuarios.ToListAsync();
+
+            foreach (var usuariobd in listaDelaBd)
+            {
+                //Mapeo de entidades
+                var newUsuarioLista = new UsuarioListaModel() {
+                    Id = usuariobd.Id,
+                    Nombres = usuariobd.Nombres,
+                    Apellidos = usuariobd.Apellidos,
+                    NombreUsuario = usuariobd.NombreUsuario,
+                    Rol = usuariobd.RolId             
+                };
+
+                respuesta.Add(newUsuarioLista);
+            }
+
+
+            return respuesta;
+        }
+
+        public async Task<UsuarioEdicionModel> ObtenerUsuarioPorId(int id)
+        {
+            var respuesta = new UsuarioEdicionModel() { };
+            var usuariodb = await _context.Usuarios.FirstOrDefaultAsync(x => x.Id == id); //Linq
+            if (usuariodb != null)
+            {
+                respuesta.Id = usuariodb.Id;
+                respuesta.Nombres = usuariodb.Nombres;
+                respuesta.Apellidos = usuariodb.Apellidos;
+                respuesta.NombreUsuario = usuariodb.NombreUsuario;
+                respuesta.RolId = usuariodb.RolId;
+            }
+
+            return respuesta;
+        }
+    
+
+        public async Task ActualizarUsuario(UsuarioEdicionModel model)
+        {
+            var usuarioDb = await _context.Usuarios.FirstOrDefaultAsync(x => x.Id == model.Id);
+            usuarioDb.Nombres = model.Nombres;
+            usuarioDb.Apellidos = model.Apellidos;
+            usuarioDb.RolId = (int)model.RolId;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task ActualizarPassword(UsuarioCambioPasswordModel model)
+        {
+            var usuarioDb = await _context.Usuarios.FirstOrDefaultAsync(x => x.Id == model.Id);
+            usuarioDb.Password = _seguridad.Encriptar(model.Password);         
             await _context.SaveChangesAsync();
         }
     }
