610c0247c77f192c1aaccb8f0488ec3136052fb3
Bruno Matus <brumatuz03@gmail.com>
2021-10-15 02:02:50 -0300

    Clase 15/10/2021 Parte Final?

---
+++ SistemaBuscador.Test/PruebasUnitarias/Controladores/UsuarioControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SistemaBuscador.Controllers;
+using SistemaBuscador.Models;
+using SistemaBuscador.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBuscador.Test.PruebasUnitarias.Controladores
+{
+    [TestClass]
+    public class UsuarioControllerTest
+    {
+        [TestMethod]
+        public async Task NuevoUsuarioExiste()
+        {
+            //Preparacion
+            var usuario = new Mock<IUsuarioRepository>();
+            var model = new UsuarioCreacionModel();
+            var controller = new UsuariosController(usuario.Object);
+
+            //Ejecucio
+            var resultado = await controller.NuevoUsuarioExiste(model) as RedirectToActionResult;
+
+            //Validacion
+            Assert.AreEqual(resultado.ActionName, "Index");
+        }
+    }
+}
---
+++ SistemaBuscador.Test/PruebasUnitarias/Servicios/UsuarioRepositoryTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SistemaBuscador.Models;
+using SistemaBuscador.Repositories;
+using SistemaBuscador.Utilidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBuscador.Test.PruebasUnitarias.Servicios
+{
+    [TestClass]
+    public class UsuarioRepositoryTest : TestBase
+    {
+        [TestMethod]
+        public async Task InsertarUsuario()
+        {
+            //Preparacion
+            var nombreBd = Guid.NewGuid().ToString();
+            var context = BuildContext(nombreBd);
+            var seguridad = new Mock<ISeguridad>();
+            seguridad.Setup(x => x.Encriptar(It.IsAny<string>())).Returns("aadbbccsss333ee55asdasda31as2d31a32sd1a32s1das3");
+            var rolRepo = new RolRepositorio(context);
+            var repo = new UsuarioRepository(context, seguridad.Object, rolRepo);
+            var modelo = new UsuarioCreacionModel() { 
+                Apellidos = "Test",
+                Nombres = "Test",
+                NombreUsuario = "Test",
+                Password = "Test",
+                RePassword = "Test",
+                RolId = 1 };
+            //Ejecucion
+            await repo.InsertatUsuario(modelo);
+            var context2 = BuildContext(nombreBd);
+            var list = await context2.Usuarios.ToListAsync();
+            var resultado = list.Count;
+            Assert.AreEqual(1, resultado);
+        }
+    }
+}
